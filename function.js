d3298e10e0b159eee2fd122764d0f1d2568f896c
MacBook Pro <gunjegunje54@gmail.com>
2024-11-06 00:10:30 +0800

    added more examples

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 // Level: easy
-// 4
+// 4.
 // function calculateProblems(count){
 //    let totalHour = count*2
 //    console.log(" i study for "+ totalHour + " hours to solve " + count + "  problems")
@@ -8,34 +8,65 @@
 // calculateProblems(5)
 // calculateProblems(9)
 
-// 5
+// 5.
 // function printFavoriteFood(name,food){
 //     console.log(name, "loves to eat", food)
 // }
 // printFavoriteFood("Sara", "pizza")
 
-// 4
+// 4.
 // function printWeather(cityName, weather){
 //     console.log(cityName, "hotod",weather,"baina" )
 // }
 // printWeather("Chicago", "cold")
 
-// 5
+// 5.
 // function printBookDetails(title, author, yearPublished){
 //     console.log(author,"wrote",title,"in", yearPublished)
 // }
 // printBookDetails("Harry Potter", "J.K. Rowling", "1997")
 
-// 6
+// 6.
 // function calculateArea(width, height){
 // area=width*height
 //     console.log("Area:"+area)
 // }
 // calculateArea(10,20)
 
-// 7
-function calculateVolume(width,height,depth){
-    area=width*height*depth
-    console.log("Volume:",area)
+// 7.
+// function calculateVolume(width,height,depth){
+//     area=width*height*depth
+//     console.log("Volume:",area)
+// }
+// calculateVolume(10,20,30)
+
+// 8.
+// function printTicketInfo(eventName,ticketPrice,discount){
+//     console.log(eventName,"iin tasalbar",ticketPrice,"tugruguus",discount,"bolj hymdarlaa")
+// }
+// printTicketInfo("Pinecone",20000,15000)
+
+
+
+// Level:medium
+// 1.
+function findDigitCount(number,digit){
+    console.log(number,'have',digit,'digits')
 }
-calculateVolume(10,20,30)
+findDigitCount(12,2)
+
+
+// 2
+// function numbers(n){
+//     console.log(n)
+// }
+// numbers(1)
+// numbers(12)
+// numbers(123)
+// numbers(1234)
+// numbers(12345)
+
+
+
+// Level:upper
+// 1
