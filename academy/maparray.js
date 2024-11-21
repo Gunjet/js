976f6f9c210b286fe863739d88f38505d5981b32
Gunjlkham08 <gunjegunje54@gmail.com>
2024-11-21 17:50:29 +0800

    added a little task

--- academy/maparray.js
+++ academy/maparray.js
@@ -7,11 +7,24 @@
 // console.log(newArr)
 
 
-const n=[81828384, 91929394]
-const newArr=n.map(func)
+// const n=[81828384, 91929394]
+// const newArr=n.map(func)
 
-function func(num){
-    return Math.floor(num / 10000)
-} 
+// function func(num){
+//     return Math.floor(num / 10000)
+// } 
 
-console.log(newArr)
+// console.log(newArr)
+
+let array=[-100, -200, 10, -10, 0, 20, 30, -3];
+
+function myFilter(currentValue, index, originalArray){
+    return currentValue>0;
+}
+
+const numberByFilter=array.filter(myFilter);
+const numberByFind=array.find(myFilter);
+const numberByFindIndex=array.findIndex(myFilter);
+
+console.log(numberByFilter);
+console.log(numberByFind);
